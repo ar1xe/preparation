ecf0b4c5d215bf2151002d514ceb305bda076ce9
ar1xe <chelseagrin52@gmail.com>
2022-11-02 20:31:53 +0300

    commit 01-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react";
+import Star from "./Star";
 
 
-function App() {
-  return (
-    <div>asd-asd</div>
-  );
-}
+const App = () => {
+    return (
+        <>
+            <div>
+                <h1>Head component</h1>
+            </div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </>
+    );
+};
 
 export default App;
---
+++ src/Star.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Star = () => {
+    return (
+        <div>
+            <span>Star</span>
+        </div>
+    );
+};
+
+export default Star;
